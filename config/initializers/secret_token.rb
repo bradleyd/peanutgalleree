5fa555281fcbb8e0749b9d27f938a399c9377977
bradley <bradleydsmith@gmail.com>
2011-10-24 18:52:03 -0400

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Upordown::Application.config.secret_token = '3e7f01d2b9f3e5bf9d50d8db2b28b4ac9eac9f782059aa136df6a154f32d88f93d4f7b9574b75694717654269bde6e94864bc1c410dff32310c3c2f76efc7e14'
