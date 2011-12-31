e37da10284488c73e8f3e1b03f7007756af9c114
brad smith <bradleydsmith@gmail.com>
2011-12-30 20:02:04 -0500

    Added attr for description

--- app/models/painting.rb
+++ app/models/painting.rb
@@ -1,6 +1,6 @@
 class Painting < ActiveRecord::Base
  #attr_accessor :remote_image_url
- attr_accessible :gallery_id, :name, :image, :remote_image_url
+ attr_accessible :gallery_id, :name, :image, :remote_image_url, :description
 
  belongs_to :gallery
  has_many :comments
